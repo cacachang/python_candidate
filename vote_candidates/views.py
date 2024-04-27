26fddc39736377852d85836b3567b78a52ac0cd2
Ning <a24701770@gmail.com>
2024-04-27 20:28:14 +0800

    add: new and create candidates

--- vote_candidates/views.py
+++ vote_candidates/views.py
@@ -1,9 +1,20 @@
-from django.http import HttpRequest
+from django.http import HttpRequest, request, HttpResponseRedirect
+from django.urls import reverse
 from django.shortcuts import render
+from .models import Candidate
 
 def new(request):
     return render(request, "new.html")
 
 def create(request):
-    
-    return render(request, "create.html")
+    if request.method == "POST":
+        candidate = Candidate(
+            name=request.POST["name"],
+            age=request.POST["age"],
+            party=request.POST["party"]
+        )
+
+        candidate.save()
+        candidates = Candidate.objects.all()
+        return render(request, "candidates/index.html", { "candidates": candidates })
+    return HttpResponseRedirect(reverse("vote_candidates:new"))
