7755b4ee1a43b127892688105d99bb560aaded3d
Ning <a24701770@gmail.com>
2024-04-27 20:55:50 +0800

    add: candidate show page

--- candidates/urls.py
+++ candidates/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('', include("vote_candidates.urls")),
-    path('candidates/index', views.index, name='index'),
+    path('<id>', views.show, name='show'),
 ]
--- candidates/views.py
+++ candidates/views.py
@@ -1,4 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from vote_candidates import models
 
 def index(request):
-    return render(request, "index.html")
+    candidates = models.Candidate.objects.all()
+    return render(request, "candidates/index.html", { "candidates": candidates } )
+
+def show(request, id):
+    try:
+        candidate = models.Candidate.objects.get(id = id)
+    except:
+        return HttpResponse("沒有這號人物", status=404)
+
+    return render(request, "candidates/show.html", { "candidate": candidate })
